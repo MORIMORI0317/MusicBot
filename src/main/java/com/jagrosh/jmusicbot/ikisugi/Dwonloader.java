db69eaf578fb78a48c54dc67e22f1662beab0397
MORIMORI <morimori.0317@outlook.jp>
2020-09-19 18:31:49 +0900

    test

--- src/main/java/com/jagrosh/jmusicbot/ikisugi/Dwonloader.java
+++ src/main/java/com/jagrosh/jmusicbot/ikisugi/Dwonloader.java
@@ -1,10 +1,18 @@
 package com.jagrosh.jmusicbot.ikisugi;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class Dwonloader {
     private static Dwonloader INSTANS;
 
     public static void init() {
         INSTANS = new Dwonloader();
+        try {
+            Paths.get("cash").toFile().createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static Dwonloader instans() {
